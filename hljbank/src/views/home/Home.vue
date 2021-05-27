adc048246e62f0cd229477b50ea1e53f58777d94
MiFan-ya <1766081937@qq.com>
2021-05-27 14:34:10 +0800

    项目创建，第一次提交

---
+++ hljbank/src/views/home/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <!-- <el-row>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"
+        ><div class="grid-content bg-purple-light"></div
+      ></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row> -->
+    <div class="e-row">
+      <el-row :gutter="10">
+        <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
+          ><div class="grid-content bg-purple-light">123</div></el-col
+        >
+        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+        <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    getdata(res) {},
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.e-row {
+  margin: 0 auto;
+  width: 1200px;
+}
+@media screen and (max-width: 500px) {
+    ::v-deep .el-col{
+      display: none;
+    }
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #cad0db;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
